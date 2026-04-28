b6480d4e9c90d6ca9bc2ca6e40be7bab888c09a1
David Marongiu <davidmarongiu@hotmail.com>
2026-04-28 16:53:14 +0200

    update worker

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// import routes
+// IMPORT ROUTES
 const expensesRoutes = require("./routes/expenses");
 
-// utiliser les routes
+// ROUTES API
 app.use("/expenses", expensesRoutes);
 
-app.listen(3001, () => {
-    console.log("Server is running on port 3001");
+// SERVIR LE FRONT (React build)
+app.use(express.static(path.join(__dirname, "dist"), {
+    setHeaders: (res, filePath) => {
+        if (filePath.endsWith(".html")) {
+            res.setHeader("Cache-Control", "no-store");
+        }
+    }
+}));
+
+// CATCH ALL (toujours en dernier)
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
-const path = require("path");
-
-// API routes AVANT
-app.use("/expenses", expensesRoutes);
-
-// Static React
-app.use(express.static(path.join(__dirname, "dist")));
-
-// Catch ALL (TOUJOURS EN DERNIER)
-app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "dist/index.html"));
+// LISTEN EN DERNIER
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
 });
